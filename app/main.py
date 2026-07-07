47e302180355e34b9d68792e8e9a28bf65a8c3c7
codingwithchitra777 <codingwithchitra@gmail.com>
2026-07-07 21:02:21 +0700

    Change default port to 8000

--- app/main.py
+++ app/main.py
@@ -118,5 +118,5 @@ else:
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", "8080"))
+    port = int(os.getenv("PORT", "8000"))
     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
