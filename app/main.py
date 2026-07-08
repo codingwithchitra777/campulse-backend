d8f53d843fa5292d3dd6ba0c5c35e8bed5e74224
Coding With Chitra <codingwithchitra@gmail.com>
2026-07-08 08:17:46 +0700

    Enable credentials in CORS middleware

--- app/main.py
+++ app/main.py
@@ -56,7 +56,7 @@ if logfire_token:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[*],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
