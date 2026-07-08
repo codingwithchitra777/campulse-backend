c6325ee472b3a20245409d5a9bfc30c2312ec4d4
Coding With Chitra <codingwithchitra@gmail.com>
2026-07-08 08:48:22 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -55,8 +55,8 @@ if logfire_token:
 # CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
